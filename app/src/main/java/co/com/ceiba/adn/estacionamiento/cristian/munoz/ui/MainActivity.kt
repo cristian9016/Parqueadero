5eb198d17072806bb86d9c360e14e346f9600fe5
Cristian David Muñoz <cristian.munoz@ceiba.com.co>
2019-08-14 08:35:41 -0500

    styles applyed in layouts

--- app/src/main/java/co/com/ceiba/adn/estacionamiento/cristian/munoz/ui/MainActivity.kt
+++ app/src/main/java/co/com/ceiba/adn/estacionamiento/cristian/munoz/ui/MainActivity.kt
@@ -2,12 +2,25 @@ package co.com.ceiba.adn.estacionamiento.cristian.munoz.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import co.com.ceiba.adn.estacionamiento.cristian.munoz.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        rgTypeVehicle.setOnCheckedChangeListener { _, i ->
+            when (i) {
+                R.id.rbCarro -> {
+                    etCilindraje.isEnabled = false
+                    etCilindraje.setText("")
+                }
+                else -> etCilindraje.isEnabled = true
+            }
+
+        }
     }
 }
